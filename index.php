338485e65db5a3f491c40520c4e08fce3633b3cc
TweaksDev22 <97637879+TweaksDev22@users.noreply.github.com>
2022-01-16 12:55:59 +0400

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+$string =  '<!DOCTYPE html>'.
+'html>'.
+'head>'.
+'meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no, minimal-ui, viewport-fit=cover">'.
+'title>TweaksDev22 Repo</title>'.
+'link rel="stylesheet" href="css/pack.css">'.
+'style>'.
+'ody, html{'.
+'margin:0px;'.
+'font-family:helvetica;'.
+'overflow-x:hidden;'.
+'height:100%;'.
+'background-color:#101010'.
+''.
+'.button{'.
+'display:inline-block;'.
+'width:80px;'.
+'padding:10px;'.
+'border-radius:5px;'.
+'background-color:#e74f4f;'.
+'margin:10px;'.
+'color:white;'.
+'text-decoration:none;'.
+'font-weight:500;'.
+''.
+'/style>'.
+'</head>'.
+'body>'.
+'<center>'.
+'<img style="width:100px;margin:10px;border-radius:20px;;margin-top:40px;" src="CydiaIcon.png">'.
+'<h1 style="margin:0px;font-size:25px;color:#fff;">TweaksDev22 Repo</h1>'.
+'<p style="margin:10px;font-size:15px;color:#fff;">My Tweaks & More..</p>'.
+'<a class="button" href="cydia://url/https://cydia.saurik.com/api/share#?source=https://tweaksdev22.github.io/Repo/">Add Repo</a>'.
+'/center>'.
+''.
+'</body>'.
+'/html>';
+
+ echo $string; 
+?>

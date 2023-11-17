4aa5c64c13f624b870af234ccb4e3f8d41693e4c
TweaksDev22 <97637879+TweaksDev22@users.noreply.github.com>
2023-11-17 12:40:23 +0000

    ini

--- index.py
+++ index.py
@@ -216,7 +216,6 @@ def main():
             DebianPackager.PushToGit()  # Push the repo to GitHub automatically.
     except Exception:
         pass
-    
     if __name__ == '__main__':
     DepictionGenerator = DepictionGenerator(version)
     PackageLister = PackageLister(version)

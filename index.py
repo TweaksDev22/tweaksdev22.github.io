39cda704386999869239128da6641e13ec07b440
TweaksDev22 <97637879+TweaksDev22@users.noreply.github.com>
2023-11-17 17:34:33 +0400

    init

--- index.py
+++ index.py
@@ -218,8 +218,4 @@ def main():
         pass
 
 
-if __name__ == '__main__':
-    DepictionGenerator = DepictionGenerator(version)
-    PackageLister = PackageLister(version)
-    DebianPackager = DebianPackager(version)
-    main()
+
